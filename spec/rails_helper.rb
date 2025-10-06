769a1c94e31b7ee996ba0a6ef12bad6320368ae4
Andrej Prastakoŭ <andrew.prostakov@gmail.com>
2025-10-06 23:48:09 +0200

    fix: basic simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,18 +1,5 @@
 # frozen_string_literal: true
 
-with_coverage = ENV.fetch('COVERAGE', false)
-
-if with_coverage
-  require 'simplecov'
-  SimpleCov.start 'rails' do
-    add_filter 'spec/'
-    add_filter 'config/'
-    add_group 'Controllers', 'app/controllers'
-    add_group 'Models', 'app/models'
-    add_group 'Services', 'app/services'
-  end
-end
-
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
@@ -22,16 +9,27 @@ require 'rspec/rails'
 require 'database_cleaner/active_record'
 require 'webmock/rspec'
 
+with_coverage = ENV.fetch('COVERAGE', false)
+if with_coverage
+  require 'simplecov'
+  SimpleCov.start do
+    coverage_dir 'spec/coverage'
+
+    add_filter 'spec/'
+    add_filter 'config/'
+
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Helpers', 'spec/helpers'
+    add_group 'Models', 'app/models'
+    add_group 'Services', 'app/services'
+  end
+end
+
 Rails.application.eager_load! if with_coverage
 
 Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # config.fixture_paths << Rails.root.join('spec/fixtures').to_s
